b7c055fc9d1037e92a1523a86d6fbd5320945a5b
srvnk <105205717+srvnk@users.noreply.github.com>
2022-06-15 20:37:53 +0530

    Add files via upload

---
+++ TelCo.ColorCoder/ReferenceManual.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using static TelCo.ColorCoder.ColorCoder;
+
+namespace TelCo.ColorCoder
+{
+    class ReferenceManual
+    {
+        public void PrintManual()
+        {
+            using (StreamWriter streamWriter = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ReferenceManual.txt")))
+            {
+                streamWriter.WriteLine("Code\tColorPair");
+                Console.WriteLine("Code\t\tColorPair");
+                for (int colorCode = 1; colorCode <= colorMapMajor.Length * colorMapMinor.Length; colorCode++)
+                {
+                    ColorPair colorPair = GetColor.GetColorFromPairNumber(colorCode);
+                    streamWriter.WriteLine(colorCode + "\t\t" + colorPair.majorColor.Name + "," + colorPair.minorColor.Name);
+                    Console.WriteLine(colorCode + "\t\t" + colorPair.majorColor.Name + "," + colorPair.minorColor.Name);
+                }
+            }
+        }
+    }
+}
